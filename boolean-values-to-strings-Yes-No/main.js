3396dea314ce3f3142a931a59650cb4953be2294
anjdtmr <anjdeitmer@gmail.com>
2022-09-03 23:43:43 +0200

    adds a challenge

---
+++ boolean-values-to-strings-Yes-No/main.js
@@ -0,0 +1,9 @@
+// INSTRUCTIONS
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// SOLUTION
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+// RESOURCES: https://stackoverflow.com/questions/22515394/how-to-convert-boolean-value-to-yes-no-while-rendering-the-flexigrid-table
