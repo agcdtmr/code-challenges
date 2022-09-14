ed574f093a0c96521be52aa9cc354209524e9383
anjdtmr <anjdeitmer@gmail.com>
2022-09-14 20:01:06 +0200

    adds a challenge

---
+++ fundamentals-return/main.js
@@ -0,0 +1,82 @@
+// INSTRUCTIONS
+//Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+// SOLUTION
+
+// function add(a,b){
+//     return a + b
+// }
+
+// let add = function(a,b) {
+//   return a + b
+// }
+
+const add = (a, b) => a + b;
+
+// function divide(a,b){
+//     return a / b
+// }
+
+// let divide = function (a,b) {
+//   return a / b
+// }
+
+const divide = (a, b) => a / b;
+
+// function multiply(a,b){
+//     return a * b
+// }
+
+// let multiply = function(a,b) {
+//   return a * b
+// }
+
+const multiply = (a, b) => a * b;
+
+// function mod(a,b){
+//     return a % b
+// }
+
+// let mod = function(a,b) {
+//   return a % b
+// }
+
+const mod = (a, b) => a % b;
+
+// function exponent(a,b) {
+//     return a ** b
+// }
+
+// let exponent = function(a,b) {
+//   return a ** b
+// }
+
+const exponent = (a, b) => a ** b;
+
+// function subt(a,b){
+//     return a - b
+// }
+
+// let subt = function (a, b) {
+//   return a - b
+// }
+
+const subt = (a, b) => a - b;
+
+// RESOURCES
