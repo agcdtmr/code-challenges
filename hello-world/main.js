6d251025f10a83af04f06ec2434e4adbeefc7374
anjdtmr <anjdeitmer@gmail.com>
2022-09-20 09:16:00 +0200

    adds a challenge

---
+++ hello-world/main.js
@@ -0,0 +1,27 @@
+// INSTRUCTIONS
+//Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// SOLUTIONS
+
+// Write a function "greet" that returns "hello world!"
+
+// function greet () {
+//  return "hello world!"
+// }
+
+// const greet = function() {
+//   return "hello world!"
+// }
+
+// const greet = () => { return "hello world!" }
+
+// const greet = () => "hello world!"
+
+const greet = () => "hello world!";
+
+// RESOURCES
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
