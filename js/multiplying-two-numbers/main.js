9f2f1a91bbda88b8a4f9096bfaad3ae2a40aac7c
anjdtmr <anjdeitmer@gmail.com>
2022-09-23 12:07:44 +0200

    adds a challenge

---
+++ js/multiplying-two-numbers/main.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS
+//Implement a function which multiplies two numbers.
+
+// SOLUTION
+// function multiply (a, b) {
+//   return a * b
+// }
+
+let multiply = function (a, b) {
+  return a * b;
+};
+
+// RESOURCES
