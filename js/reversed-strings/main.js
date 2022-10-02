96e882c48e7934c6c49aa4dfe4611eb95844c995
anjdtmr <anjdeitmer@gmail.com>
2022-10-02 15:55:58 +0200

    adds a challenge

---
+++ js/reversed-strings/main.js
@@ -0,0 +1,19 @@
+// INSTRUCTIONS
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// SOLUTION
+
+function solution(str) {
+  //   const strArr = Array.from(str)
+  //   const reversed = strArr.reverse()
+  //   const test = reversed.join("")
+  //   return test
+  return Array.from(str).reverse().join("");
+}
+
+// RESOURCES
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
