245ae90c96933ff7b116b2a68a2e69f9fe28c072
anjdtmr <anjdeitmer@gmail.com>
2022-10-11 10:08:15 +0200

    adds a challenge

---
+++ python/arithmetic-operations.py
@@ -0,0 +1,45 @@
+# INSTRUCTIONS
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+
+
+# Print the following:
+
+# 8
+# -2
+# 15
+# Input Format
+
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+
+
+
+# SOLUTION
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+sum = a + b
+print(sum)
+
+diff = a - b
+print(diff)
+
+prod = a * b
+print(prod)
+
+
+# RESOURCES
+
+# https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+#SAMPLE TEST
+
