fb000513a4e7f4810d30976e8420c2f9a2d5aa4b
anjdtmr <anjdeitmer@gmail.com>
2022-10-12 11:21:10 +0200

    adds a challenge

---
+++ python/check-avg.py
@@ -0,0 +1,14 @@
+# INSTRUCTIONS
+
+# Write a function which calculates the average of the numbers in a given list.
+
+# Note: Empty arrays should return 0.
+
+# SOLUTION
+
+def find_average(numbers):
+    return sum(numbers) / len(numbers)
+
+# RESOURCES
+
+# https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/
