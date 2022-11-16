5652b2bd9453d734ef0cc246211393e20a02a3d5
adtmr <anjdeitmer@gmail.com>
2022-11-16 08:29:49 +0100

    adds a challenge

---
+++ python/str-to-num.py
@@ -0,0 +1,34 @@
+# INSTRUCTIONS
+
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+# SOLUTION
+
+def number_to_string(num):
+    return str(num)
+
+# RESOURCES
+
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+#SAMPLE TEST
+
+import codewars_test as test
+from solution import number_to_string
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(number_to_string(67), '67')
+        test.assert_equals(number_to_string(79585), '79585')
+        test.assert_equals(number_to_string(79585), "79585")
+        test.assert_equals(number_to_string(1+2), '3')
+        test.assert_equals(number_to_string(1-2), '-1')
