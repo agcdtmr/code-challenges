b6d2c3098af33f39ab91d880b94fdd5fe6ef5998
adtmr <anjdeitmer@gmail.com>
2023-03-01 08:46:09 +0100

    Add parsing.py

---
+++ python/parsing.py
@@ -0,0 +1,38 @@
+# INSTRUCTIONS
+
+'''
+Here is a piece of code:
+
+def get_status(is_busy):
+    status = "busy" if is_busy else "available"
+    return status
+Expected Behaviour
+Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+'''
+
+# SOLUTION
+
+def get_status(is_busy):
+    return {"status": ("available", "busy")[is_busy]}
+
+# RESOURCES
+
+#https://www.codewars.com/kata/54fdaa4a50f167b5c000005f/
+
+# SAMPLE TEST
+
+
+'''
+import codewars_test as test
+from solution import get_status
+
+@test.describe("Basic Tests")
+def basic_tests():
+    
+    @test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(get_status(True)["status"], "busy")
+        test.assert_equals(get_status(False)["status"], "available")
+'''
